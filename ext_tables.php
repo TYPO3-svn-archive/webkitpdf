afccac05e8ae99a97a70c22b0ceec4c1dbd29808
xonx <xonx@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-20 19:42:17 +0000

    Fixed #6136: Exclude startingpoint field from plugin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/webkitpdf/trunk@29116 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -7,7 +7,7 @@ if(!defined('TYPO3_MODE')) {
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'WebKit PDF');
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,pages,select_key';
 
 t3lib_extMgm::addPlugin(array('LLL:EXT:webkitpdf/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
 
